1d630c15e5c750e7c3c6e086b5b2359a3d727874
mineyas <mineyaskha@gmail.com>
2022-02-16 16:49:32 +0100

    React Native quete 1

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import  CameraScreen  from "./screens/CameraScreen";
+import  FeedScreen  from "./screens/FeedScreen";
+import  ImagesScreen  from "./screens/ImagesScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Camera" component={CameraScreen} options={{ unmountOnBlur: true }} />
+        <Tab.Screen name="Feed" component={FeedScreen} />
+        <Tab.Screen name="Images" component={ImagesScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
