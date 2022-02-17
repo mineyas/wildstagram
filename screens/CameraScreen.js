a70f65dcf78c33ff6284bffce4b35b43ed892428
mineyas <mineyaskha@gmail.com>
2022-02-17 14:59:49 +0100

    React Native 04 - Affichage des photos

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import * as ImageManipulator from "expo-image-manipulator";
 import { Camera } from "expo-camera";
 
 export default function CameraScreen() {
@@ -22,11 +23,18 @@ export default function CameraScreen() {
   return (
     <>
       <Camera style={styles.camera} ref={cameraRef} />
-      <Button title="Press me" 
-      onPress={async() => {
-        const pictureMetadata = await cameraRef.current.takePictureAsync();
-        console.log('pictureMetadata', pictureMetadata)
-      }} />
+      <Button
+        title="Press me"
+        onPress={async () => {
+          const pictureMetadata = await cameraRef.current.takePictureAsync();
+          console.log("pictureMetadata", pictureMetadata);
+          console.log(
+            await ImageManipulator.manipulateAsync(pictureMetadata.uri, [
+              { resize: { width: 800 } },
+            ])
+          );
+        }}
+      />
     </>
   );
 }
--- screens/ImagesScreen.js
+++ screens/ImagesScreen.js
@@ -1,7 +1,27 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, Image } from "react-native";
+import * as FileSystem from "expo-file-system";
 
 export default function ImagesScreen() {
-  return <Text>Yooo Images</Text>;
+  const [imagesURI, setImagesURI] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const images = await FileSystem.readDirectoryAsync(
+        FileSystem.cacheDirectory + "ImageManipulator"
+      );
+      setImagesURI(images);
+    })();
+  }, []);
+  return imagesURI.length > 0 ? (
+    <Image
+      style={{
+        flex: 1,
+        resizeMode: "cover",
+        height: 500,
+      }}
+      source={{
+        uri: FileSystem.cacheDirectory + "ImageManipulator/" + imagesURI[0],
+      }}
+    />
+  ) : null;
 }
-
